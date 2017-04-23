daeef8645fd23c61e592e8470c9bfb3d00dffe23
Snolting <s.l.nolting@gmail.com>
2017-04-23 16:30:58 -0400

    font choice

--- app/pdfs/survey_pdf.rb
+++ app/pdfs/survey_pdf.rb
@@ -76,7 +76,7 @@ class SurveyPdf < Prawn::Document
 
   def content
 
-    font "Times-Roman", :style => :normal, :size => 12
+    font "Courier", :style => :normal, :size => 12
 
     start_new_page(:margin => [72, 90])
     table_of_contents
