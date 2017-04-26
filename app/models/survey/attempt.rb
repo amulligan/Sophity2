b04d9b8464e3edeb9f0b6b816be10b5697e9164d
Snolting <s.l.nolting@gmail.com>
2017-04-26 19:16:07 -0400

    try to order top concerns by score value

--- app/models/survey/attempt.rb
+++ app/models/survey/attempt.rb
@@ -59,7 +59,7 @@ class Survey::Attempt < ActiveRecord::Base
             join survey_surveys s on sq.survey_id = s.id
              join survey_attempts sat on sa.attempt_id = sat.id
              where sa.option_id =3 and sat.participant_id = ? and s.id= ?", self.participant_id, self.survey.id]).map(&:text)]
-      ]
+      ].flatten(1)
      if self.grade.include? 'A'
        if top_concerns_list.empty?
         return ["N/A"]
