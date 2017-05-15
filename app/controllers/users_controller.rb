766fa7d0e0ca8310d571989741588f047e00c3f2
Snolting <s.l.nolting@gmail.com>
2017-05-15 17:28:46 -0400

    test to see if edit works in users_controller.rb 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,20 +40,20 @@ def change_name
 
   def edit
     @user = User.find(params[:id])
-    @user.update_attributes(user_params)
   end
 
   def update
     @user = User.find(params[:id])
-    @user.report_requested = true
-    params[:report_requested] = true
-    if @user.update_attributes(user_params)
-      attachment = generate_pdf
-      UserNotifier.send_signup_email(@current_user, attachment).deliver_now
-      UserNotifier.send_admin_report(@current_user, attachment).deliver_now
-      redirect_to view_report_path(participant_id: @user.id)
-    else
-      render 'edit'
+    # @user.report_requested = true
+    if params[:report_requested] == true
+      if @user.update_attributes(user_params)
+        attachment = generate_pdf
+        UserNotifier.send_signup_email(@current_user, attachment).deliver_now
+        UserNotifier.send_admin_report(@current_user, attachment).deliver_now
+        redirect_to view_report_path(participant_id: @user.id)
+      else
+        render 'edit'
+      end
     end
   end
 
