d2485bdd542f1ff10e2b9cdcf3f95031ab4e5294
Snolting <s.l.nolting@gmail.com>
2017-05-15 17:55:00 -0400

    try to get views/sessions/new.html.erb to recognize email param

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,21 @@
 class SessionsController < ApplicationController
   def new
+    email = params[:email]
+
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user
-      log_in user     
+      log_in user
       Survey::Attempt.where(participant_id: current_user.id).destroy_all
-     
-    else     
+
+    else
       @user = User.new(email: params[:session][:email].downcase)
       @user.save
-      log_in @user    
-    
-    end  
+      log_in @user
+
+    end
     redirect_to new_attempt_path(participant_id: current_user.id, survey_id:1)
   end
 
